4f7adad8db061eddeaf87ac2ebf9a05cdbb9ae92
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2024-06-05 20:06:15 +0700

    Add Lesson5

---
+++ project/src/main/java/ru/istomin/lesson4/UserRepository.java
@@ -0,0 +1,11 @@
+package ru.istomin.lesson4;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
